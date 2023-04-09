a60ba859ba43829035c2ce7ae605ece30c108e50
bhattapradip271@gmail.com <bhattaramchandra686@gmail.com>
2023-04-09 15:20:10 +0545

    add arithmitic

---
+++ arithmetic.py
@@ -0,0 +1,13 @@
+# program to find addition multiplication subtraction division and modulus
+a=int(input('enter a number:'))
+b=int(input('enter b number:'))
+c=a+b;
+d=a*b
+e=a-b;
+f=a/b
+g=a%b
+print("The sum of a and b is:",c)
+print("The multiplication of two num:",d)
+print("The subtraction of two number is:",e)
+print("The division of two number is:",f)
+print("The modulos of two number is:",g)
